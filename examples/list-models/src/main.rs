143db51b29b7d9de68286a925c1d97a8ae48e59e
Katsuhiro Honda <freewave3@gmail.com>
2025-01-13 18:53:50 +0900

    Add anthropic-sdk and tokio dependencies for list_models example

--- examples/list-models/src/main.rs
+++ examples/list-models/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anthropic_sdk::clients::AnthropicClient;
+use anthropic_sdk::types::model::ModelClient;
+
+#[tokio::main]
+async fn main() {
+    let client = AnthropicClient::new(
+        "sk-ant-api03-000000000000000000000000-0000000000",
+        "2024-06-01",
+    )
+    .unwrap();
+
+    let models = client.list_models(None).await.unwrap();
+    println!("{:?}", models);
 }
